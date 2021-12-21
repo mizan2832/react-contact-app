685fa246b5491e3aac3fdffb2d580e2e5930c000
mizan2832 <programmer620@gmail.com>
2021-12-21 10:00:37 +0600

    contact app

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function contactList() {
+    return (
+        <div className='ui celled list'>
+            contact list
+        </div>
+    )
+}
+
+export default contactList
